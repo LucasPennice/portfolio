8861bf8e7a2f892848ad44aa990ef9f38a02c201
Lucas <lucaspennice@gmail.com>
2021-12-21 11:50:32 -0300

    added custom favicon, added function that tells you what tab are you on

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,47 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default ({ switchTheme }) => {
+	const [currentPage, setCurrentPage] = useState('/');
 	const onAnchorClick = (e) => {
 		console.log(e);
+	};
+	const onWhatSection = (id) => {
+		if (id !== currentPage) setCurrentPage(id);
+	};
+
+	const isCurrentPage = (id) => {
+		if (currentPage === id) return 'currentClass';
+		return '';
 	};
 	return (
 		<nav className="navBar">
 			<div className="selectedDiv"></div>
-			<a href="#home">home</a>
-			<Link to="/" onClick={(e) => onAnchorClick(e)}>
+			<Link
+				to="/"
+				onClick={(e) => {
+					onAnchorClick(e);
+					onWhatSection('/');
+				}}
+				className={isCurrentPage('/')}
+			>
 				projects
 			</Link>
-			<Link to="/about">about</Link>
-			<Link to="/contact">contact</Link>
+			<Link
+				to="/about"
+				id="/about"
+				className={isCurrentPage('/about')}
+				onClick={() => onWhatSection('/about')}
+			>
+				about
+			</Link>
+			<Link
+				to="/contact"
+				className={isCurrentPage('/contact')}
+				onClick={() => onWhatSection('/contact')}
+			>
+				contact
+			</Link>
 		</nav>
 	);
 };
